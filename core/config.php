590ff3f23070914dc9aa5f9f7a85b11c1dedb5ae
ProjectCoreDevs <projectcore.devs@gmail.com>
2018-01-07 20:13:45 +0100

    Update config
    temporary add english language, before implement language change system

--- core/config.php
+++ core/config.php
@@ -1,12 +1,12 @@
-<?php
+<?php include 'languages/english.php';
     // Database Configuration
     $_dbhost = '127.0.0.1';					// host
     $_dbuser = 'root';						// database username
     $_dbpass = 'password';					// database password
 	
-	$web_database = 'cms_database';			// Name of the website database
-	$auth_database = 'auth';				// Name of the 'auth' database
-	$characters_database = 'characters';	// Name of the 'characters' database
+	$web_database = 'cms_database';			// website database
+	$auth_database = 'auth';				// auth database
+	$characters_database = 'characters';	// characters database
     
     $db = new mysqli($_dbhost, $_dbuser, $_dbpass) or die('MySQL Error');
 
